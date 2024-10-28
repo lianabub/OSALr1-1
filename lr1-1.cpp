b33a55943f9c36cd965080c04eaf22d8caa6f6a4
liana <lianabub07@gmail.com>
2024-10-28 12:37:24 +0300

    Add files via upload

---
+++ lr1-1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    double z1, z2, b, a, pi = 3.1415926;
+    std::cout << "Введите a \n";
+    std::cin >> a;
+    std::cout << "Введите b \n";
+    std::cin >> b;
+    z1 = cos(a) + sin(a) + cos(3 * a) + sin(3 * a);
+    z2 = 2 * sqrt(2) * cos(a) * sin(((pi * 0.25) + 2 * a));
+    std::cout << " z1 = " << z1 << " ; z2 = " << z2;
+    return 0;
+}
+
